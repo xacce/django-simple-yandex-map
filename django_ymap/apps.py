a7a04825ca17a24fd0d008e51f9aa32625dd2737
Xacce <thisice@gmail.com>
2017-10-12 14:14:55 +0300

    https://model-mommy.readthedocs.io/en/latest/ support

--- django_ymap/apps.py
+++ django_ymap/apps.py
@@ -1,6 +1,15 @@
-__author__ = 'winzard'
 from django.apps.config import AppConfig
 
 
 class SimpleYandexMap(AppConfig):
     name = "django_ymap"
+    def ready(self):
+        try:
+            from model_mommy.generators import add
+        except ImportError:
+            pass
+        else:
+            from random import randint
+            from django_ymap.fields import YmapCoord
+            add(YmapCoord,lambda: '{},{}'.format(randint(-50,50),randint(-50,50)))
+
